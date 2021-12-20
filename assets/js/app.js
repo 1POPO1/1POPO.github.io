e83fe57f382ddc7ac79e5b576ba2c170f87141f0
1POPO1 <96410408+1POPO1@users.noreply.github.com>
2021-12-20 18:13:13 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,17 +41,6 @@ $(function() {
     });
 
 
-
-    /* Menu nav toggle */
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
-    });
-
-
-
     /* Collapse */
     $("[data-collapse]").on("click", function(event) {
         event.preventDefault();
